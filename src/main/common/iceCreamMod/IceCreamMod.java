306c13ba9cb279802e6e82b1e7d30581e8555c37
civilwargeeky <civilwargeeky@yahoo.com>
2014-06-15 00:35:22 -0500

    Renamed Example Mod to IceCreamMod

---
+++ src/main/common/iceCreamMod/IceCreamMod.java
@@ -0,0 +1,11 @@
+package iceCreamMod;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = IceCreamMod.MODID, version = IceCreamMod.VERSION)
+public class IceCreamMod
+{
+    public static final String MODID = "icecreammod";
+    public static final String VERSION = "1.0";
+    
+}
