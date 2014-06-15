15ba5ea3d243e73f43ba5d3f02cdf80a4c14df7e
civilwargeeky <civilwargeeky@yahoo.com>
2014-06-15 00:43:21 -0500

    Renaming, added lib

---
+++ src/main/common/icecreammod/IceCreamMod.java
@@ -0,0 +1,9 @@
+package icecreammod;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = icecreammod.lib.ModInfo.MODID)
+public class IceCreamMod
+{
+    
+}
---
+++ src/main/common/icecreammod/lib/ModInfo.java
@@ -0,0 +1,6 @@
+package icecreammod.lib;
+
+public class ModInfo {
+	public static final String MODID = "icecreammod";
+	public static final String a = "";
+}
