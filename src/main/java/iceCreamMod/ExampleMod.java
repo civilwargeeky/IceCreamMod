e09fb147794b4287e1ab16098dfee907ddd68b6e
civilwargeeky <civilwargeeky@yahoo.com>
2014-06-14 23:52:14 -0500

    Moving things around

---
+++ src/main/java/iceCreamMod/ExampleMod.java
@@ -0,0 +1,11 @@
+package iceCreamMod;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+}
