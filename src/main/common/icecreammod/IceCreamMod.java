7d150eb14d6bd096a2df2a661a120ed664fd83fb
civilwargeeky <civilwargeeky@yahoo.com>
2014-06-15 00:53:39 -0500

    Renamed some things, added to lib

--- src/main/common/icecreammod/IceCreamMod.java
+++ src/main/common/icecreammod/IceCreamMod.java
@@ -2,8 +2,8 @@ package icecreammod;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = icecreammod.lib.ModInfo.MODID)
+@Mod(modid = icecreammod.lib.ModInfo.MOD_ID, name = icecreammod.lib.ModInfo.MOD_NAME, version = icecreammod.lib.ModInfo.DEFAULT_VERSION)
 public class IceCreamMod
 {
-    
+
 }
--- src/main/common/icecreammod/lib/ModInfo.java
+++ src/main/common/icecreammod/lib/ModInfo.java
@@ -1,6 +1,7 @@
 package icecreammod.lib;
 
 public class ModInfo {
-	public static final String MODID = "icecreammod";
-	public static final String a = "";
+	public static final String MOD_ID = "icecreammod";
+	public static final String MOD_NAME = "Ice Cream Mod";
+	public static final String DEFAULT_VERSION = "1.0";
 }
