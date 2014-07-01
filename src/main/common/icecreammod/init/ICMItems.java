029f4e71637ba3aedb950722f12d9233ccf498f9
civilwargeeky <civilwargeeky@yahoo.com>
2014-07-01 16:10:46 -0500

    Added in all the items

--- src/main/common/icecreammod/init/ICMItems.java
+++ src/main/common/icecreammod/init/ICMItems.java
@@ -8,21 +8,31 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 
 public class ICMItems {
-	private static float average(float par1, float par2) {
-		return (par1+par2)/2;
+	private static float averageSat(BaseFood par1, BaseFood par2) { //This is a weighted average for calculating saturation
+		return (par1.saturation+par2.saturation)*3/4;
 	}
 	
 	public static BaseItem spoon = new BaseItem("Spoon");
 	public static BaseFood cone = new BaseFood("Cone", 1, 0.5F);
+	public static BaseFood chocolateCracker = new BaseFood("Chocolate Cracker", 1, 0.1F);	
 	
-	public static IceCreamBase iceCreamBase = new IceCreamBase("Ice Cream", 0, 0);
-	public static IceCream iceCreamCone = new IceCream("Ice Cream Cone", iceCreamBase.heals, average(iceCreamBase.saturation, cone.saturation));
+	public static IceCreamBase iceCreamBase = new IceCreamBase("Ice Cream", 3, 0.8F);
+	public static IceCream iceCreamCone = new IceCream("Ice Cream Cone", iceCreamBase.heals+cone.heals, averageSat(iceCreamBase, cone));
+	public static IceCream iceCreamSandwich = new IceCream("Ice Cream Sandwich", iceCreamBase.heals + 2*chocolateCracker.heals, averageSat(iceCreamBase, chocolateCracker));
+	public static IceCream iceCreamBowl = new IceCream("Ice Cream Bowl", iceCreamBase.heals, iceCreamBase.saturation);
+	
+	public static BaseFood debugItem = (BaseFood) new BaseFood("Debug Item", -10, 0).setAlwaysEdible();
 	
 	public static void init() {
 		GameRegistry.registerItem(spoon, spoon.name);
 		GameRegistry.registerItem(cone, cone.name);
+		GameRegistry.registerItem(chocolateCracker, chocolateCracker.name);
 		GameRegistry.registerItem(iceCreamBase, iceCreamBase.name);
 		GameRegistry.registerItem(iceCreamCone, iceCreamCone.name);
+		GameRegistry.registerItem(iceCreamSandwich, iceCreamSandwich.name);
+		GameRegistry.registerItem(iceCreamBowl, iceCreamBowl.name);
+		
+		GameRegistry.registerItem(debugItem, debugItem.name);
 		
 	}
 }
--- src/main/common/icecreammod/lib/Reference.java
+++ src/main/common/icecreammod/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public Reference() {
 	}
 
-	public static String[] typeNames = {"Vanilla", "Chocolate", "Strawberry"};
+	public static String[] typeNames = {"Vanilla", "Chocolate"}; //Maybe add in Strawberry if some mod is added with strawberries
 
 	public static String sanitize(String string) { //Makes first letter lower case, then removes all spaces.
 		return string.substring(0,1).toLowerCase() + string.replaceAll("\\s","").substring(1);
